5c4a0aa2fea7d54dd4843f41db66028911f38aa7
szeru@mum.edu <szeru@mum.edu>
2023-01-08 12:49:20 -0800

    add extra space

--- src/main/java/com/saminium/tutorials/authorizationserver/config/SecurityConfig.java
+++ src/main/java/com/saminium/tutorials/authorizationserver/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
             throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
                 .oidc(Customizer.withDefaults()); // Enable OpenID Connect 1.0
 
